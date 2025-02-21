1908f3067095606bbde6e58e7f1fff2cf6ae6a05
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-21 16:31:34 -0600

    Will create tests for Cell class and complete iteration 1

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe Cell do
     @cell = Cell.new("B4")
     @cruiser = Ship.new("Cruiser", 3)
   end 
+
   describe '#initialize' do
     it 'exists' do
-      cell = Cell.new("B4")
-      expect(cell).to be_a(Cell)
+      expect(@cell).to be_a(Cell)
     end
   end
 
@@ -33,6 +33,45 @@ RSpec.describe Cell do
       @cell.place_ship(@cruiser)
       expect(@cell.ship).to eq(@cruiser)
       expect(@cell.empty?).to eq(false)
+    end
+  end
+
+  describe '#fired_upon?' do
+    it 'checks if a cell has been fired upon' do
+      @cell.place_ship(@cruiser)
+      expect(@cell.fired_upon?).to eq(false)
+    end
+  end
+
+  describe '#fire_upon' do
+    it 'fires upon a ship' do
+      @cell.place_ship(@cruiser)
+      @cell.fire_upon
+      expect(@cell.ship.health).to eq(2)
+      expect(@cell.fired_upon?).to eq(true)
+    end
+  end
+
+  describe '#render' do
+    it 'renders correctly before and after firing' do
+      cell_1 = Cell.new("B4")
+      expect(cell_1.render).to eq(".")
+      cell_1.fire_upon
+      expect(cell_1.render).to eq("M")
+
+      cell_2 = Cell.new("C3") 
+      cell_2.place_ship(@cruiser)
+      expect(cell_2.render).to eq(".")
+
+      expect(cell_2.render(true)).to eq("S")
+      cell_2.fire_upon
+      expect(cell_2.render).to eq("H")
+
+      expect(@cruiser.sunk?).to eq(false)
+      @cruiser.hit
+      @cruiser.hit
+      expect(@cruiser.sunk?).to eq(true)
+      expect(cell_2.render).to eq("X")
     end
   end
 end
