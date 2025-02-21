f90add29737bc3d4cdc424be5efcf7111d3a4a08
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-21 16:30:33 -0600

    Create a cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,6 +3,7 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -12,4 +13,31 @@ class Cell
   def place_ship(ship)
     @ship = ship
   end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    return if fired_upon? 
+    @fired_upon = true
+
+    if @ship
+      @ship.hit
+    end
+  end
+
+  def render(reveal_ship = false)
+    if @fired_upon
+      if @ship && @ship.sunk?
+        return "X"
+      elsif @ship
+        return "H"
+      else
+        return "M"
+      end
+    end
+    return "S" if reveal_ship && @ship #revisit
+    "." 
+  end             
 end
