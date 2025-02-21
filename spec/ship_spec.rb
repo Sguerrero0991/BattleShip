7a1c418ec4c9df2cc5bf94b5892414109d0c9034
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-20 20:22:44 -0500

    Adds tests for ship class

---
+++ spec/ship_spec.rb
@@ -0,0 +1,55 @@
+require './lib/ship'
+require 'pry'
+RSpec.describe Ship do
+
+    describe '#inialize' do
+    
+    #add before each do block w cruiser
+        it 'exists' do
+            cruiser = Ship.new("Cruiser", 3)
+
+            expect(cruiser).to be_a(Ship)
+        end
+
+        it 'has attributes' do #split up
+            cruiser = Ship.new("Cruiser", 3)
+
+            expect(cruiser.name).to eq("Cruiser")
+            expect(cruiser.length).to eq(3)
+            expect(cruiser.health).to eq(3)
+        end
+    end
+
+    describe '#sunk?' do
+        it 'defaults to false' do
+            cruiser = Ship.new("Cruiser", 3)
+
+            expect(cruiser.sunk?).to eq false
+        end
+    end
+
+    describe '#hit' do
+        it 'can be hit and lose health' do
+            cruiser = Ship.new("Cruiser", 3)
+
+            expect(cruiser.health).to eq(3)
+
+            cruiser.hit
+
+            expect(cruiser.health).to eq(2)
+        end
+
+        it 'can be sunk' do
+            cruiser = Ship.new("Cruiser", 3)
+
+            expect(cruiser.health).to eq(3)
+
+            cruiser.hit
+            cruiser.hit
+            cruiser.hit
+
+            expect(cruiser.health).to eq(0)
+            expect(cruiser.sunk?).to eq true
+        end
+    end
+end
