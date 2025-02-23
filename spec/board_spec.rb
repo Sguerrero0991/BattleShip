a67a2e1bad920288c877a62f8294c6e8629094e3
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-22 19:40:12 -0600

    Added some changes

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -89,6 +89,7 @@ RSpec.describe Board do
 
       expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
       expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+      #hopefully this works 
     end
   end
 end 
