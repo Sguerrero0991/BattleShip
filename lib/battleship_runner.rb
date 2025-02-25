96052199e04f37e8fdf08db4c619430cc74223f6
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-25 15:35:43 -0600

    Added to runner.rb file and finished game class

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -22,5 +22,23 @@ def main_menu
 end
 
 def play_game
-    game = Game.new # Initialize the logic for the game
+    game = Game.new # Create a new game instance 
+
+    puts "Computer is placing ships ..."
+    game.place_computer_ships
+    puts "Computer ships placed"
+
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts game.player_board.render(false)
+    game.place_player_ships
+
+    turn = Turn.new(game) #starts the game loop
+    turn.play
+
+
+
 end
+
+
